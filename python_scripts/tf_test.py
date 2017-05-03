c7952b8456c309d343b947a273f7182e760f727c
Stephen Inglis <dartonias@gmail.com>
2017-05-03 15:45:22 -0400

    Trying to list tables after connection

--- python_scripts/tf_test.py
+++ python_scripts/tf_test.py
@@ -15,7 +15,8 @@ conn = psycopg2.connect(
 )
 
 with conn.cursor() as cur:
-  cur.execute("SELECT * FROM remax_listings;")
+  #cur.execute("SELECT * FROM remax_listings;")
+  cur.execute("SELECT * FROM pg_catalog.pg_tables;")
   results = cur.fetchall()
   for r in results:
     print(r)
