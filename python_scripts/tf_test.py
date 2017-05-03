95943e7d66919ac337dda2b42133c70f13466e1e
Stephen Inglis <dartonias@gmail.com>
2017-05-03 14:49:22 -0400

    Added database connection testing

--- python_scripts/tf_test.py
+++ python_scripts/tf_test.py
@@ -1,4 +1,18 @@
 import tensorflow as tf
+import os
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 hello = tf.constant('Hello, TensorFlow!')
 
